7cf7b0d58df61221ea357349fe9e33cab5d80abd
Ashoka Reddy Manyam <ashokareddy.manyam@ad.infosys.com>
2020-08-06 18:06:19 +0530

    updated

--- movierating/src/main/java/com/movierating/api/config/JpaConfiguration.java
+++ movierating/src/main/java/com/movierating/api/config/JpaConfiguration.java
@@ -2,10 +2,8 @@ package com.movierating.api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Component
 @Configuration
 @EnableJpaRepositories("com.movierating.api.repositories")
 @EnableTransactionManagement
